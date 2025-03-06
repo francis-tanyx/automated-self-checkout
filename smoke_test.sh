d3108227dbd80fb749a380847773e81fdba2f6c7
Yan Xue, Tan (Francis) <118970371+francis-tanyx@users.noreply.github.com>
2025-03-06 17:01:39 +0800

    Update smoke_test.sh

--- smoke_test.sh
+++ smoke_test.sh
@@ -103,7 +103,7 @@ teardown
 
 # 4. Yolov8s pipeine: should see non-empty pipeline0.log contents
 echo "Running YOLOv8s pipeline with logs..."
-INPUTSRC=https://github.com/intel-iot-devkit/sample-videos/raw/master/people-detection.mp4 PIPELINE_SCRIPT=yolov8s_roi.sh docker compose -f src/docker-compose.yml up -d
+INPUTSRC=https://github.com/intel-iot-devkit/sample-videos/raw/master/people-detection.mp4 PIPELINE_SCRIPT=yolov8s_roi.sh docker compose -f src/docker-compose-yolov8s.yml up -d
 status_code=$?
 verifyStatusCode $status_code 
 # allowing some time to process
@@ -113,7 +113,7 @@ teardown
 
 # 5. Age pipeline: should see non-empty pipeline0.log contents
 echo "Running Age Classification pipeline with logs..."
-INPUTSRC=https://www.pexels.com/download/video/3248275 PIPELINE_SCRIPT=age_recognition.sh docker compose -f src/docker-compose.yml up -d
+INPUTSRC=https://www.pexels.com/download/video/3248275 PIPELINE_SCRIPT=age_recognition.sh docker compose -f src/docker-compose-yolov8s.yml up -d
 status_code=$?
 verifyStatusCode $status_code 
 # allowing some time to process
